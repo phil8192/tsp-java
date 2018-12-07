e23d80e2964fdbacda49e99d2a80cf9cf05f5117
phil8192 <phil@parasec.net>
2018-12-07 22:56:02 +0000

    tsp solver factory

--- src/main/java/net/parasec/tsp/TSPSolver.java
+++ src/main/java/net/parasec/tsp/TSPSolver.java
@@ -1,14 +1,20 @@
 package net.parasec.tsp;
 
-import net.parasec.tsp.impl.FLS;
 import net.parasec.tsp.impl.Point;
+import net.parasec.tsp.impl.FLS;
 import net.parasec.tsp.impl.MutateFLS;
+import net.parasec.tsp.impl.GLS;
 
-public final class TSPSolver implements TSP {
-    public double solve(final Point[] points) {
-        //final FLS fls = new FLS();
-        //return fls.optimise(points);
-        MutateFLS mutateFLS = new MutateFLS();
-        return mutateFLS.optimise(points);
+public class TSPSolver {
+  public double solve(Point[] points, String solver) {
+    TSP tspSolver;
+    if(solver.equals("fls")) {
+      tspSolver = new FLS();
+    } else if(solver.equals("mutate_fls")) {
+      tspSolver = new MutateFLS();
+    } else if(Sovler.equals("gls_fls")) {
+      tspSolver = new GLS();
     }
+  }
+  return tspSovler.optimise(points);
 }
