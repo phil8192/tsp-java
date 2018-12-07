1a604a7ca75efc6587a73692c3482fb4f7fd0a5b
phil8192 <phil@parasec.net>
2018-12-07 23:46:13 +0000

    oh dear

--- src/main/java/net/parasec/tsp/TSPMain.java
+++ src/main/java/net/parasec/tsp/TSPMain.java
@@ -3,20 +3,12 @@ package net.parasec.tsp;
 import net.parasec.tsp.impl.Point;
 
 public final class TSPMain {
-    // args[0] = .tsp
-    // args[1] output:
-    //   each line = vertex
-    //   line>line = edge.
-    public static void main(final String[] args) {
-	final Point[] points = PointsReader.read(args[0]); 
-	final TSP tsp = new TSPSolver();
-	
-	// error = distance found.
-	final long l = System.currentTimeMillis();
-	final double error = tsp.solve(points);
-	
-	DumpPoints.dump(points, args[1]);
-
-	System.out.printf("tour length = %.4f\toptimisation time = %.2f seconds.\n", error, (System.currentTimeMillis() - l)/1000d);	
-    }
+  public static void main(String[] args) {
+    Point[] points = PointsReader.read(args[0]);
+    TSP tsp = new TSPSolver();
+    double error = tsp.solve(points, "mutate_fls");
+    DumpPoints.dump(points, args[1]);
+    System.out.printf("tour length = %.4f\toptimisation time = %.2f seconds.\n", +
+        error, (System.currentTimeMillis() - l)/1000d);
+  }
 }
--- src/main/java/net/parasec/tsp/TSPSolver.java
+++ src/main/java/net/parasec/tsp/TSPSolver.java
@@ -7,14 +7,17 @@ import net.parasec.tsp.impl.GLS;
 
 public class TSPSolver {
   public double solve(Point[] points, String solver) {
+    if(solver == null) throw new IllegalArgumentException();
     TSP tspSolver;
     if(solver.equals("fls")) {
       tspSolver = new FLS();
     } else if(solver.equals("mutate_fls")) {
       tspSolver = new MutateFLS();
-    } else if(Sovler.equals("gls_fls")) {
+    } else if(sovler.equals("gls_fls")) {
       tspSolver = new GLS();
+    } else {
+      throw new Exception(solver + " not recognised.");
     }
+    return tspSovler.optimise(points);
   }
-  return tspSovler.optimise(points);
 }
