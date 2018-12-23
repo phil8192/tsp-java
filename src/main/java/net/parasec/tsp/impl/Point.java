178c07ccf9758f3e95892fb7f8e5ce790a23166f
phil8192 <phil@parasec.net>
2018-12-23 19:56:45 +0000

    distance range

--- src/main/java/net/parasec/tsp/impl/Point.java
+++ src/main/java/net/parasec/tsp/impl/Point.java
@@ -5,8 +5,8 @@ public final class Point {
   private int id;
   private final double x;
   private final double y;
-  //private boolean active = true;
-  private boolean active = false;
+  private boolean active = true;
+  //private boolean active = false;
 
   private final boolean prime;
   private boolean _isPrime(int n) {
@@ -56,7 +56,13 @@ public final class Point {
   public static double distance(final Point[] points) {
     final int len = points.length;
     double d = points[len-1].distance(points[0]);
-    for(int i = 1; i < len; i++) {
+    d += distance(points, 0, len - 1);
+    return d;
+  }
+
+  public static double distance(Point[] points, int from, int to) {
+    double d = 0d;
+    for(int i = from+1; i <= to; i++) {
       //d += points[i-1].distance(points[i]);
       final Point pre = points[i-1], cur = points[i];
       double distance = pre.distance(cur);
