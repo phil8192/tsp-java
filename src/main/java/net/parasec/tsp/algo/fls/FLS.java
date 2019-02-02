d660824dcfdad08ff7faccc499d758397807e3bd
phil8192 <phil@parasec.net>
2019-02-02 00:55:28 +0000

    refactor

--- src/main/java/net/parasec/tsp/algo/fls/FLS.java
+++ src/main/java/net/parasec/tsp/algo/fls/FLS.java
@@ -19,27 +19,27 @@ public class FLS implements TSP {
     this.twoOptMoveCost = twoOptMoveCost;
   }
 
-/**
-  * 2-Opt a tour.
-  * removes 2 edges, then reconstructs a tour.
-  * in general (from http://en.wikipedia.org/wiki/2-opt):
-  *   1. take route[0] to route[i-1] and add them in order to new_route
-  *   2. take route[i] to route[k] and add them in reverse order to new_route
-  *   3. take route[k+1] to end and add them in order to new_route
-  *
-  * note that reverse is called with min(from,to)+1, max(from,to). in a
-  * 2-opt move, 2 edges are removed, leaving 2 disconnected sub-tours.
-  * either one of these subtours is then reversed and the 2 subtours
-  * reconnected in a different (shorter) way.
-  *
-  * Bentley, in his experiements on TSP heuristics paper notes
-  * that since either subtour can be reversed, it is best to
-  * reverse the shortest one, otherwise an arbitrary reversal
-  * will be N/2 array accesses.
-  *
-  * this implementation currently reverses whatever subtour does
-  * not wrap around -which could be the larger of the two.
-  */
+  /**
+   * 2-Opt a tour.
+   * removes 2 edges, then reconstructs a tour.
+   * in general (from http://en.wikipedia.org/wiki/2-opt):
+   * 1. take route[0] to route[i-1] and add them in order to new_route
+   * 2. take route[i] to route[k] and add them in reverse order to new_route
+   * 3. take route[k+1] to end and add them in order to new_route
+   * <p>
+   * note that reverse is called with min(from,to)+1, max(from,to). in a
+   * 2-opt move, 2 edges are removed, leaving 2 disconnected sub-tours.
+   * either one of these sub-tours is then reversed and the 2 subtours
+   * reconnected in a different (shorter) way.
+   * <p>
+   * Bentley, in his experiments on TSP heuristics paper notes
+   * that since either sub-tour can be reversed, it is best to
+   * reverse the shortest one, otherwise an arbitrary reversal
+   * will be N/2 array accesses.
+   * <p>
+   * this implementation currently reverses whatever subtour does
+   * not wrap around -which could be the larger of the two.
+   */
   private void reverse(final Point[] x, final int from, final int to) {
     for(int i = from, j = to; i < j; i++, j--) {
       final Point tmp = x[i];
@@ -49,37 +49,39 @@ public class FLS implements TSP {
   }
 
   /**
-  * a tour is a circle. wrap around.
-  */
+   * a tour is a circle. wrap around.
+   */
   private int wrap(final int i, final int max) {
-    return (max+i) % max;
+    return (max + i) % max;
   }
 
   /**
-  * set active bits for 4 vertices making up edges ab, cd.
-  */
+   * set active bits for 4 vertices making up edges ab, cd.
+   */
   private void activate(final Point a, final Point b,
                         final Point c, final Point d) {
-    a.setActive(true); b.setActive(true);
-    c.setActive(true); d.setActive(true);
+    a.setActive(true);
+    b.setActive(true);
+    c.setActive(true);
+    d.setActive(true);
   }
 
   /**
-  * try to find a move from the current city.
-  * given the current city, search for a 2-opt move that will result in
-  * an improvement to the tour length. the edge before the current city,
-  * (prevPoint,currenPoint) and after (currentPoint,nextPoint) are compared
-  * to all over edges (c,d), starting at (c=currentPoint+2, d=currentPoint+3)
-  * until an improvement is found.
-  */
+   * try to find a move from the current city.
+   * given the current city, search for a 2-opt move that will result in
+   * an improvement to the tour length. the edge before the current city,
+   * (prevPoint,currentPoint) and after (currentPoint,nextPoint) are compared
+   * to all over edges (c,d), starting at (c=currentPoint+2, d=currentPoint+3)
+   * until an improvement is found.
+   */
   private double findMove(final int current, final Point currentPoint,
                           final Point[] points, final int numCities) {
 
     final TwoOptMoveCost twoOptMoveCost = this.twoOptMoveCost;
 
     // previous and next city index and point object.
-    final int prev = wrap(current-1, numCities);
-    final int next = wrap(current+1, numCities);
+    final int prev = wrap(current - 1, numCities);
+    final int next = wrap(current + 1, numCities);
     final Point prevPoint = points[prev];
     final Point nextPoint = points[next];
 
@@ -87,9 +89,9 @@ public class FLS implements TSP {
     // until i = current+numCities-2, j = current+numCities-1.
     // if points = {0,1,2,3,4,5,6,7,8,9}, current = 4, this will produce:
     // (6,7) (7,8) (8,9) (9,0) (0,1) (1,2) (2,3)
-    for(int i = wrap(current+2, numCities), j = wrap(current+3, numCities);
+    for(int i = wrap(current + 2, numCities), j = wrap(current + 3, numCities);
         j != current;
-        i = j, j = wrap(j+1, numCities)) {
+        i = j, j = wrap(j + 1, numCities)) {
 
       final Point c = points[i];
       final Point d = points[j];
@@ -106,7 +108,7 @@ public class FLS implements TSP {
       if(delta1 < 0) {
         activate(prevPoint, currentPoint, c, d);
         //System.out.println("reverse from = " + (Math.min(prev, i)+1) + " to = " + Math.max(prev, i));
-        reverse(points, Math.min(prev, i)+1, Math.max(prev, i));
+        reverse(points, Math.min(prev, i) + 1, Math.max(prev, i));
         return delta1;
       }
 
@@ -122,7 +124,7 @@ public class FLS implements TSP {
       if(delta2 < 0) {
         activate(currentPoint, nextPoint, c, d);
         //System.out.println("reverse from = " + (Math.min(prev, i)+1) + " to = " + Math.max(prev, i));
-        reverse(points, Math.min(current, i)+1, Math.max(current, i));
+        reverse(points, Math.min(current, i) + 1, Math.max(current, i));
         return delta2;
       }
 
@@ -131,9 +133,9 @@ public class FLS implements TSP {
   }
 
   /**
-  * optimise a tour.
-  * return a 2-Optimal tour.
-  */
+   * optimise a tour.
+   * return a 2-Optimal tour.
+   */
   public double optimise(final Point[] points, double score) {
 
     // total number of cities in the tour
@@ -156,18 +158,16 @@ public class FLS implements TSP {
         // if a move was found, go to previous city.
         // best is += modified delta.
         if(modified < 0) {
-          current = wrap(current-1, numCities);
+          current = wrap(current - 1, numCities);
           visited = 0;
           score += modified;
-          //System.out.printf("fls score = %.4f.\n", score);
-          //System.out.printf("verify    = %.4f.\n", Point.distance(points));
           continue;
         }
         currentPoint.setActive(false);
       }
 
       // if city is inactive or no moves found, go to next city.
-      current = wrap(current+1, numCities);
+      current = wrap(current + 1, numCities);
       visited++;
     }
     return score;
