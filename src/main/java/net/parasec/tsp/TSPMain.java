837193f31425d19ad9cad798a45316325114f7fc
phil8192 <phil@parasec.net>
2019-02-02 00:13:29 +0000

    general idea

--- src/main/java/net/parasec/tsp/TSPMain.java
+++ src/main/java/net/parasec/tsp/TSPMain.java
@@ -1,11 +1,41 @@
 package net.parasec.tsp;
 
+import net.parasec.tsp.impl.*;
+
 public class TSPMain {
+
   public static void main(String[] args) {
-    String algo = args[0];
-    String input = args[1];
-    String output = args[2];
 
+    String input = args[0];
+    String output = args[1];
+    String algo = args[2];
 
+    PointsReader.PointParser<Point> pointParser = new PointsReader.PointParser<Point>() {
+      @Override
+      public Point parse(String[] line) {
+        return new Point(Integer.parseInt(line[0]),
+            Double.parseDouble(line[1]),
+            Double.parseDouble(line[2]));
+      }
+    };
+
+    PointsReader<Point> pointPointsReader = new PointsReader<>();
+    Point[] points = pointPointsReader.read(input, pointParser);
+
+    TourDistance<Point> tourDistance = new DefaultDistance();
+    double initialScore = tourDistance.distance(points);
+
+    TwoOptMoveCost twoOptMoveCost = new BasicTwoOptMoveCost();
+    TSP tsp = new FLS(twoOptMoveCost);
+
+    if(algo.equals("gls_fls")) {
+      int maxRuns = Integer.parseInt(args[3]);
+      PenaltyMatrix penaltyMatrix = new ArrayPenaltyMatrix(points.length);
+      tsp = new GLS(tourDistance, tsp, penaltyMatrix, maxRuns, output);
+    }
+
+    double newScore = tsp.optimise(points, initialScore);
+
+    DumpPoints.dump(points, output);
   }
 }
