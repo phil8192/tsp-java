029ae498bb5d5d3a759598cd37f744b7aaf10d38
phil8192 <phil@parasec.net>
2019-02-02 00:58:23 +0000

    refactor

--- src/main/java/net/parasec/tsp/algo/gls/BFPM.java
+++ src/main/java/net/parasec/tsp/algo/gls/BFPM.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 // memory mapped.
+// idea from vanilla java:
+// http://vanillajava.blogspot.com/2011/12/using-memory-mapped-file-for-huge.html
+
 public class BFPM implements PenaltyMatrix {
   private static final int MAPPING_SIZE = 1 << 30; // 1024*1024*1024 bytes/1 gig.
   private final RandomAccessFile raf;
@@ -20,22 +23,22 @@ public class BFPM implements PenaltyMatrix {
     this.lru = new LRUCache(numCities * 1000);
     this.raf = new RandomAccessFile(file, "rw");
     try {
-      long size = 2L * numCities*(numCities-1) / 2; // triangle - diag.
-      for (long offset = 0; offset < size; offset += MAPPING_SIZE) {
+      long size = 2L * numCities * (numCities - 1) / 2; // triangle - diag.
+      for(long offset = 0; offset < size; offset += MAPPING_SIZE) {
         long size2 = Math.min(size - offset, MAPPING_SIZE);
         mappings.add(raf.getChannel()
             .map(FileChannel.MapMode.READ_WRITE, offset, size2));
       }
-    } catch (IOException e) {
+    } catch(IOException e) {
       e.printStackTrace();
     }
   }
 
   private long position(final int x, final int y) {
     final int i = Math.min(x, y), j = Math.max(x, y);
-    final long n = i+1;
-    final long offset = i*numCities + j;
-    return offset - n*(n+1)/2; // rm diag + triangle.
+    final long n = i + 1;
+    final long offset = i * numCities + j;
+    return offset - n * (n + 1) / 2; // rm diag + triangle.
   }
 
   public int getPenalty(final int i, final int j) {
@@ -45,11 +48,10 @@ public class BFPM implements PenaltyMatrix {
       if(lru.size() < lru.getMax()) {
         penalty = 0;
       } else {
-    final int mapN = (int) (p / MAPPING_SIZE);
-    final int offN = (int) (p % MAPPING_SIZE);
-    //return mappings.get(mapN).getShort(offN);
-    penalty = mappings.get(mapN).getShort(offN);
-    lru.put(p, penalty);
+        final int mapN = (int) (p / MAPPING_SIZE);
+        final int offN = (int) (p % MAPPING_SIZE);
+        penalty = mappings.get(mapN).getShort(offN);
+        lru.put(p, penalty);
       }
     }
     return penalty;
@@ -68,9 +70,8 @@ public class BFPM implements PenaltyMatrix {
 
   public void clear() {
     lru.clear();
-    for(ByteBuffer b: mappings) {
+    for(ByteBuffer b : mappings) {
       b.clear();
     }
-    System.out.println("penalties cleared.");
   }
 }
